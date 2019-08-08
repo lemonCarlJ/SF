6c54ac470165b818b618a66173be9e1705e8f540
lemonCarlJ <rpgshifa@aliyun.com>
2019-08-08 13:53:57 +0800

    console修改

--- SF/SF/console.php
+++ SF/SF/console.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * console.php
- * @Copyright lemonCarlJ All rights reserved.
+ * @Copyright LemonCarlJ All rights reserved.
  * @License http://www.56code.com/License.txt
  * @Version 1.0.0
  * @Email rpgshifa@aliyun.com
@@ -11,36 +11,107 @@ namespace SF;
 class console
 {
     /**
-     * @var string|mixed $content 控制台内容
+     * @var array 输入内容
      */
-    protected $content = '';
+    protected $content = array();
 
     /**
-     * @var mixed $command 执行命令
+     * @var string 命令
      */
-    protected $command;
+    protected $commond = '';
 
     /**
-     * @var mixed $arguments 执行参数
+     * @var array 参数
      */
-    protected $arguments;
+    protected $parameter = array();
 
     /**
      * 控制台模块初始化
-     * @param string $content 输入控制台内容
-     * @param Closure|callable $command_analysis 执行命令分析
-     * @param Closure|callable $arguments_analysis 执行参数分析
+     * @param array $content 输入内容
+     * @param string $commond_identify 命令标识符 [ r ]
+     * @param string $parameter_identify 参数标识符 [ p ]
      */
-    public function __construct( $content, $command_analysis, $arguments_analysis )
+    public function __construct( $content, $commond_identify = 'r', $parameter_identify = 'p' )
     {
-        //控制台内容
+        //命令内容
         $this->content = $content;
 
-        //解析执行命令
-        $this->command = $command_analysis( $content );
+        //获取命令
+        $this->commond = $this->commond( $commond_identify );
 
-        //解析执行参数
-        $this->arguments = $arguments_analysis( $content );
+        //获取参数
+        $this->parameter = $this->parameter( $parameter_identify );
+    }
+
+    /**
+     * 获取命令
+     * @param string $identify 命令标识符
+     * @return string 返回获取命令
+     */
+    protected function commond( $identify )
+    {
+        //初始化命令
+        $commond = '';
+
+        //获取命令
+        if( isset( $this->content[ $identify ] ) )
+        {
+            $commond = $this->content[ $identify ];
+
+            //字符转义
+            $commond = addslashes( $commond );
+
+            //去除标签
+            $commond = htmlspecialchars( $commond );
+            $commond = strip_tags( $commond );
+
+            //去除特殊字符
+            $commond = trim( $commond );
+
+            //去除空格
+            $commond = preg_replace('/\s/', '', $commond );
+        }
+
+        return $commond;
+    }
+
+    protected function parameter( $identify )
+    {
+        //初始化参数
+        $parameter = array();
+
+        if( isset( $this->content[ $identify ] ) )
+        {
+            $parameter = trim( $this->content[ $identify ], '/' );
+
+            if( is_string( $parameter ) && strpos( $parameter, '/' ) )
+            {
+                $parse = '';
+
+                //分割参数并获取列表
+                $lists = explode( '/', $parameter );
+
+                //过滤操作
+                $lists = array_filter( $lists );
+
+                //重新排列
+                $lists = array_values( $lists );
+
+                for( $i = 0; $i < count( $lists ); $i ++ )
+                {
+                    if( $i % 2 == 0 )
+                    {
+                        $parse .= '&' . $lists[ $i ] . '=';
+                    }else{
+                        $parse .= $lists[ $i ];
+                    }
+                }
+
+                parse_str( $parse, $parameter );
+            }
+        }
+
+        return $parameter;
     }
 
     public function __get( $name )
